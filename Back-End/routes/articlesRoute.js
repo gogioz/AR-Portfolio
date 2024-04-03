619be25ebc714406e51f195533faffed80a0a8a7
gogioz <fathihagar0@gmail.com>
2024-04-04 00:20:31 +0200

    fix the destination

--- Back-End/routes/articlesRoute.js
+++ Back-End/routes/articlesRoute.js
@@ -8,7 +8,7 @@ const router = express.Router();
 // Set up Multer
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../DashBoard/imgs/Articles");
+    cb(null, "imgs/");
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
